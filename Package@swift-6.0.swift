cf43a4101d339c1f453f879d7aabbcd0c6cdde04
Nakcheon Jung <ncjung@photowidget.net>
2024-11-22 23:34:23 +0900

    fix

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -21,14 +21,14 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.0.0"),
+    .package(url: "https://github.com/photowidget/swift-concurrency-extras-1.3.0", branch: "release/1.3.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2"),
   ],
   targets: [
     .target(
       name: "Clocks",
       dependencies: [
-        .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras"),
+        .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras-1.3.0"),
         .product(name: "IssueReporting", package: "xctest-dynamic-overlay"),
       ]
     ),
